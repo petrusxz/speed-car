0d5a11d7cd141207aeeeb9145f6ab81e7108ad8d
Petrus Cyrino <petrusxz@gmail.com>
2018-05-23 22:21:19 -0300

    Update app.firebase.config.ts

--- src/app/app.firebase.config.ts
+++ src/app/app.firebase.config.ts
@@ -1,8 +1,8 @@
 export const FIREBASE_CONFIG = {
-    apiKey: 'AIzaSyAP0Dm8Us4ZZS2fzht46z7wUUUMu6C8nmw',
-    authDomain: 'speedcar-autocenter.firebaseapp.com',
-    databaseURL: 'https://speedcar-autocenter.firebaseio.com',
-    projectId: 'speedcar-autocenter',
-    storageBucket: 'speedcar-autocenter.appspot.com',
-    messagingSenderId: '8853876140'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
 }
